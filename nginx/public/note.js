682078fc30c9dcfbdef3869e3467f67d2382306c
Carlos Torres <carlosfrtr@gmail.com>
2024-01-06 11:32:21 -0300

    Ajustes

--- nginx/public/note.js
+++ nginx/public/note.js
@@ -1,34 +1,18 @@
 var note = {
-    update_time: 2000,
-    save: false,
-    load: function() {
-       document.getElementById("note").focus();
-       document.getElementById("note").addEventListener("keydown", function(e) {e.keyCode == 9? false: true;})
-       document.getElementById("note").addEventListener('keyup', function(e) { note.changed = true});
-       window.setInterval(function() { note.changed ? note.save() : note.update(); }, note.update_time);
-    },
+  update_time: 2000,
+  save: false,
+  load: function() {
+      document.getElementById("note").focus();
+      window.setInterval(function() { note.save() }, note.update_time);
+  },
+  save: function() {
+      xmlDoc = new XMLHttpRequest();
+      xmlDoc.open('PUT', '', true);
+      xmlDoc.send(document.getElementById("note").value);
+  }};
 
-   save: function() {
-       xmlDoc = new XMLHttpRequest();
-       xmlDoc.open('PUT', '', true);
-       xmlDoc.send(document.getElementById("note").value);
-       note.changed = false;
-   },
-
-   update: function() {
-       xmlDoc = new XMLHttpRequest();
-       xmlDoc.open('POST', '', true);
-       xmlDoc.onload = function(e) {
-           if (xmlDoc.response != null)
-               document.getElementById("note").value = JSON.parse(xmlDoc.response).note;
-       }
-       xmlDoc.send();
-   }
-
-  };
-
-   document.onreadystatechange = () => {
-     if (document.readyState === 'complete') {
-       note.load();
-     }
-   };
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    note.load();
+  }
+};
--- src/note.py
+++ src/note.py
@@ -14,21 +14,18 @@ redis_instance = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS
 def init():
     return render_template('index.html')
 
-
-@app.route('/<path:url>', methods=['GET', 'PUT', 'POST'])
+@app.route('/<path:url>', methods=['GET', 'PUT'])
 def update(url):
     note = request.data.decode(UTF_8)
 
     if request.method == 'PUT':
         redis_instance.set(url, note.encode(UTF_8))
-    else:
-        note = redis_instance.get(url)
-        if note is not None:
-            note = note.decode(UTF_8)
-        else:
-            note = ''
-
-    if request.method == 'PUT' or request.method == 'POST':
         return jsonify({'note': note})
-    if request.method == 'GET':
-        return render_template('note.html', note=note, resource=("http://"+request.host+":81"))
+    
+    note = redis_instance.get(url)
+    if note is not None:
+        note = note.decode(UTF_8)
+    else:
+        note = ''
+
+    return render_template('note.html', note=note, resource=("http://"+request.host+":81"))
